4f41041b5b81267edbbe882aa4dd44e118bbb166
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 20:09:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   new_order = sentence.split(" ")
-  new_order.collect do
-     
+  new_order.collect do |word|
+    word.reverse 
+  end 
   new_order = new_order.join(" ")
 end 
