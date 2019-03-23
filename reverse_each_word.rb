60824648889987ee3945df9f0258f969185cc61c
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 20:13:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  new_order = []
+  new_array = []
   sentence_array.collect do |word|
-    new_order << word.reverse 
+    new_array << word.reverse 
   end 
-  new_order.join(" ")
+  new_array.join(" ")
 end 
